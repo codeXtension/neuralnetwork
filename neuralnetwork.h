103c6780e7fc9f05662b598a3bc4ae3f1ffeabb8
Elie Khoury <elie.kh@gmail.com>
2018-03-12 15:59:53 +0100

    first commit

---
+++ neuralnetwork.h
@@ -0,0 +1,403 @@
+//
+// Created by elie on 07.03.18.
+//
+
+#ifndef NEURALNETWORK_SYNAPSE_H
+#define NEURALNETWORK_SYNAPSE_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include "utilities.h"
+#include <list>
+#include <map>
+#include <sstream>
+
+
+namespace cx {
+    using namespace std;
+
+    class neuralnetwork;
+
+    class neuron {
+    protected:
+        double value;
+        string id;
+    private:
+        vector<neuralnetwork> incoming_synapse;
+        vector<neuralnetwork> outgoing_synapse;
+    public:
+        neuron();
+
+        neuron(string id);
+
+        virtual vector<neuralnetwork> &getIncoming_synapse();
+
+        bool operator==(neuron &rhs);
+
+        bool operator!=(neuron &rhs);
+
+        virtual void setIncoming_synapse(vector<neuralnetwork> &incoming_synapse);
+
+        vector<neuralnetwork> &getOutgoing_synapse();
+
+        void setOutgoing_synapse(vector<neuralnetwork> &outgoing_synapse);
+
+        double getValue();
+
+        virtual void setValue(double value);
+
+        string &getId();
+
+        void setId(string &id);
+
+        virtual double activationValue();
+
+        virtual double activationPrimeValue();
+    };
+
+    class bias_neuron : public neuron {
+    public:
+        bias_neuron();
+
+        explicit bias_neuron(string id);
+
+        double activationValue() override;
+
+        double activationPrimeValue() override;
+
+        void setValue(double value) override;
+
+        void setIncoming_synapse(vector<neuralnetwork> &incoming_synapse) override;
+    };
+
+    class neuralnetwork {
+
+    private:
+        string id;
+        double weight;
+        neuron *source;
+        neuron *target;
+
+    public:
+        neuralnetwork();
+
+        neuralnetwork(double weight, neuron &source, neuron &target);
+
+        neuron *getSource();
+
+        void setSource(neuron *source);
+
+        neuron *getTarget();
+
+        void setTarget(neuron *target);
+
+        string &getId();
+
+        void setId(string &id);
+
+        double getWeight();
+
+        void setWeight(double weight);
+
+        bool operator==(neuralnetwork &rhs);
+
+        bool operator!=(neuralnetwork &rhs);
+    };
+
+    class brain {
+    private:
+        map<int, std::list<neuron>> layers;
+        vector<double> expected_output_values;
+
+        void create_synapses();
+
+    public:
+        brain(int in_size, int out_size, int nb_hidden_layers, int hidden_layer_size, bool with_bias);
+
+        void load(data_holder &test_data_holder, bool ignore_weights);
+
+        data_holder &unload();
+
+        list <neuron> &get_layer(int layer_nb);
+
+        map<string, double> &actual_weights();
+
+        map<int, std::list<neuron>> &getLayers();
+
+        void setLayers(map<int, std::list<neuron>> &layers);
+
+        vector<double> &getExpected_output_values();
+
+        void setExpected_output_values(vector<double> &expected_output_values);
+    };
+
+    bias_neuron::bias_neuron() {
+        this->value = 1;
+    }
+
+    void bias_neuron::setIncoming_synapse(vector<neuralnetwork> &incoming_synapse) {
+
+    }
+
+    void bias_neuron::setValue(double value) {
+
+    }
+
+    double bias_neuron::activationPrimeValue() {
+        return neuron::getValue();
+    }
+
+    double bias_neuron::activationValue() {
+        return neuron::getValue();
+    }
+
+    bias_neuron::bias_neuron(string id) : neuron(id) {
+        this->id = id;
+    }
+
+    neuron::neuron() {
+    }
+
+    vector<neuralnetwork> &neuron::getIncoming_synapse() {
+        return incoming_synapse;
+    }
+
+    void neuron::setIncoming_synapse(vector<neuralnetwork> &incoming_synapse) {
+        neuron::incoming_synapse = incoming_synapse;
+    }
+
+    vector<neuralnetwork> &neuron::getOutgoing_synapse() {
+        return outgoing_synapse;
+    }
+
+    void neuron::setOutgoing_synapse(vector<neuralnetwork> &outgoing_synapse) {
+        neuron::outgoing_synapse = outgoing_synapse;
+    }
+
+    double neuron::getValue() {
+        return value;
+    }
+
+    void neuron::setValue(double value) {
+        neuron::value = value;
+    }
+
+    string &neuron::getId() {
+        return id;
+    }
+
+    void neuron::setId(string &id) {
+        neuron::id = id;
+    }
+
+    bool neuron::operator==(neuron &rhs) {
+        return id == rhs.id;
+    }
+
+    bool neuron::operator!=(neuron &rhs) {
+        return !(rhs == *this);
+    }
+
+    double neuron::activationPrimeValue() {
+        if (!this->incoming_synapse.empty()) {
+            return derivativeSigmoid(value);
+        } else {
+            return value;
+        }
+    }
+
+    double neuron::activationValue() {
+        if (!this->incoming_synapse.empty()) {
+            return sigmoid(value);
+        } else {
+            return value;
+        }
+    }
+
+    neuron::neuron(string id) {
+        this->id = id;
+    }
+
+
+    neuralnetwork::neuralnetwork() {
+        this->id = "-";
+        this->weight = 0;
+        this->source = new neuron("");
+        this->target = new neuron("");
+    }
+
+    bool neuralnetwork::operator==(neuralnetwork &rhs) {
+        return id == rhs.id;
+    }
+
+    bool neuralnetwork::operator!=(neuralnetwork &rhs) {
+        return rhs.id != this->id;
+    }
+
+    string &neuralnetwork::getId() {
+        return id;
+    }
+
+    void neuralnetwork::setId(string &id) {
+        neuralnetwork::id = id;
+    }
+
+    double neuralnetwork::getWeight() {
+        return weight;
+    }
+
+    void neuralnetwork::setWeight(double weight) {
+        neuralnetwork::weight = weight;
+    }
+
+    neuron *neuralnetwork::getSource() {
+        return source;
+    }
+
+    void neuralnetwork::setSource(neuron *source) {
+        neuralnetwork::source = source;
+    }
+
+    neuron *neuralnetwork::getTarget() {
+        return target;
+    }
+
+    void neuralnetwork::setTarget(neuron *target) {
+        neuralnetwork::target = target;
+    }
+
+    neuralnetwork::neuralnetwork(double weight, neuron &source, neuron &target) {
+        this->weight = weight;
+        this->source = &source;
+        this->target = &target;
+        this->source->getOutgoing_synapse().push_back(*this);
+        this->target->getIncoming_synapse().push_back(*this);
+    }
+
+
+    map<int, std::list<neuron>> &brain::getLayers() {
+        return layers;
+    }
+
+    void brain::setLayers(map<int, std::list<neuron>> &layers) {
+        brain::layers = layers;
+    }
+
+    vector<double> &brain::getExpected_output_values() {
+        return expected_output_values;
+    }
+
+    void brain::setExpected_output_values(vector<double> &expected_output_values) {
+        brain::expected_output_values = expected_output_values;
+    }
+
+    void brain::create_synapses() {
+        for (int i = 0; i < this->layers.size() - 1; i++) {
+            list <neuron> sources = layers.at(i);
+            list <neuron> targets = layers.at(i + 1);
+            for (neuron &source : sources) {
+                for (neuron target : targets) {
+                    double value = (rand() * 78 + 20) / 100;
+                    if (neuron *v = dynamic_cast<neuron *>(&target)) {
+                        new neuralnetwork(value, source, target);
+                    }
+                }
+            }
+        }
+    }
+
+    brain::brain(int in_size, int out_size, int nb_hidden_layers, int hidden_layer_size, bool with_bias) {
+        layers.clear();
+
+        list <neuron> neurons;
+
+        for (int j = 0; j < in_size; j++) {
+            neurons.emplace_back(neuron("N1." + (j + 1)));
+        }
+        layers.insert(pair<int, std::list<neuron>>(0, neurons));
+
+        for (int i = 1; i < nb_hidden_layers + 1; i++) {
+            neurons.clear();
+            if (with_bias) {
+                stringstream id;
+                id << "BN" << (i + 1) << ".1";
+                neurons.emplace_back(bias_neuron(id.str()));
+            }
+            for (int j = 0; j < hidden_layer_size; j++) {
+                stringstream id;
+                id << "N" << (i + 1) << "." << (j + (with_bias ? 1 : 0) + 1);
+                neurons.emplace_back(neuron(id.str()));
+            }
+            layers.insert(pair<int, std::list<neuron>>(i, neurons));
+        }
+
+        neurons.clear();
+        for (int j = 0; j < out_size; j++) {
+            stringstream id;
+            id << "N" << (nb_hidden_layers + 2) << "." << (j + 1);
+            neurons.emplace_back(neuron(id.str()));
+        }
+        layers.insert(pair<int, std::list<neuron>>(nb_hidden_layers + 1, neurons));
+
+        create_synapses();
+    }
+
+    void brain::load(data_holder &test_data_holder, bool ignore_weights) {
+        this->expected_output_values = test_data_holder.getExpected_outputs();
+
+        for (int i = 0; i < layers.size(); i++) {
+            list <neuron> sources = layers.at(i);
+            for (neuron source : sources) {
+                if (test_data_holder.getValues().count(source.getId()) > 0) {
+                    source.setValue(test_data_holder.getValues().at(source.getId()));
+                }
+                if (!ignore_weights) {
+                    for (neuralnetwork synapse : source.getOutgoing_synapse()) {
+                        if (test_data_holder.getWeights().count(synapse.getId()) > 0) {
+                            synapse.setWeight(test_data_holder.getWeights().at(synapse.getId()));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    data_holder &brain::unload() {
+        data_holder dataHolder;
+
+        for (int i = 0; i < layers.size(); i++) {
+            list <neuron> sources = layers.at(i);
+            for (neuron source : sources) {
+                dataHolder.getValues().insert(pair<string, double>(source.getId(), source.getValue()));
+                for (neuralnetwork synapse : source.getOutgoing_synapse()) {
+                    dataHolder.getWeights().insert(pair<string, double>(synapse.getId(), synapse.getWeight()));
+                }
+            }
+        }
+
+        dataHolder.setExpected_outputs(getExpected_output_values());
+
+        return dataHolder;
+    }
+
+    list <neuron> &brain::get_layer(int layer_nb) {
+        return this->layers.at(layer_nb);
+    }
+
+    map<string, double> &brain::actual_weights() {
+        map<string, double> results;
+        for (int i = 0; i < layers.size() - 1; i++) {
+            list <neuron> sources = layers.at(i);
+            for (neuron source : sources) {
+                for (neuralnetwork synapse : source.getOutgoing_synapse()) {
+                    results.insert(pair<string, double>(synapse.getId(), synapse.getWeight()));
+                }
+            }
+        }
+        return results;
+    }
+
+}
+
+#endif //NEURALNETWORK_SYNAPSE_H
---
+++ utilities.h
@@ -0,0 +1,94 @@
+//
+// Created by elie on 07.03.18.
+//
+
+#ifndef NEURALNETWORK_UTILITIES_H
+#define NEURALNETWORK_UTILITIES_H
+
+#include <cmath>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+namespace cx {
+    double sigmoid(double value) {
+        return 1.0f / (1 + exp(-value));
+    }
+
+    double derivativeSigmoid(double value) {
+        double sigmoid = cx::sigmoid(value);
+        return sigmoid * (1.0d - sigmoid);
+    }
+
+    enum value_tyle {
+        INPUT,
+        OUTPUT
+    };
+
+    enum method_type {
+        SGD,
+        BATCH,
+        MINI_BATCH
+    };
+
+    class data_holder {
+    public:
+         map<string, double> &getWeights() ;
+        void setWeights( map<string, double> &weights);
+         map<string, double> &getValues() ;
+        void setValues( map<string, double> &values);
+         vector<double> &getExpected_outputs() ;
+        void setExpected_outputs( vector<double> &expected_outputs);
+         vector<double> &getPrev_outputs() ;
+        void setPrev_outputs( vector<double> &prev_outputs);
+        void add_input( vector<double> &inputs);
+
+    private:
+        map<string, double> weights;
+        map<string, double> values;
+        vector<double> expected_outputs;
+        vector<double> prev_outputs;
+    };
+
+     map<string, double> &data_holder::getWeights()  {
+        return weights;
+    }
+
+    void data_holder::setWeights( map<string, double> &weights) {
+        data_holder::weights = weights;
+    }
+
+     map<string, double> &data_holder::getValues()  {
+        return values;
+    }
+
+    void data_holder::setValues( map<string, double> &values) {
+        data_holder::values = values;
+    }
+
+     vector<double> &data_holder::getExpected_outputs()  {
+        return expected_outputs;
+    }
+
+    void data_holder::setExpected_outputs( vector<double> &expected_outputs) {
+        data_holder::expected_outputs = expected_outputs;
+    }
+
+     vector<double> &data_holder::getPrev_outputs()  {
+        return prev_outputs;
+    }
+
+    void data_holder::setPrev_outputs( vector<double> &prev_outputs) {
+        data_holder::prev_outputs = prev_outputs;
+    }
+
+    void data_holder::add_input( vector<double> &inputs) {
+        for (int i=0; i<inputs.size(); i++) {
+            double input = inputs.at(i);
+            this->values.insert(pair<string, double>("N1." + i, input));
+        }
+    }
+}
+#endif //NEURALNETWORK_UTILITIES_H
