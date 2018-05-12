4b7ceb7910e08b31608ee8f59d3d1950a45597dd
Elie Khoury <elie@el-khoury.net>
2018-05-12 19:33:36 +0200

    pass by ref now works

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ using namespace cx;
 
 int main() {
     neural_network network = neural_network(true, 0.002, SGD, 2,1,1,3);
-
     return 0;
 }
+
+
 
--- utilities.h
+++ utilities.h
@@ -10,6 +10,7 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <fstream>
 
 using namespace std;
 
